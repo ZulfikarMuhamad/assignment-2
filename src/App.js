5cc2ba0ddf9f5936314500f4aa233d10d63e2079
juldaus <zulfikar.muhamad16@gmail.com>
2024-01-15 10:04:56 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+// CurrencyExchange.js
+import React, { useState, useEffect } from 'react';
 import './App.css';
+  
+const CurrencyExchange = () => {
+    const [exchangeRates, setExchangeRates] = useState([]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const fetchExchangeRates = async () => {
+      fetch('https://api.currencyfreaks.com/latest?apikey=ee828769239847918e93ad5876aa7593', {
+        method: 'GET'
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Connection Error')
+        }
+        return response.json()
+      })
+      .then(data => {
+        setExchangeRates(Object.entries(data.rates))
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
 
-export default App;
+    useEffect(() => {
+        fetchExchangeRates();
+    }, []);
+
+    const filteredCurrencies = ['CAD', 'IDR', 'JPY', 'CHF', 'EUR', 'GBP', 'USD'];
+
+    return (
+        <>
+          <div>
+              <h1>Currency Exchange Rates (Based on 1 USD)</h1>
+              <table>
+              <thead>
+                  <tr>
+                  <th>Currency</th>              
+                  <th>We Buy</th>
+                  <th>Exchange Rate</th>
+                  <th>We Sell</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {exchangeRates
+                  .filter(([currency]) => filteredCurrencies.includes(currency))
+                  .map(([currency, rate]) => {
+                      const exchangeRate = Number(rate);
+                      const weBuy = exchangeRate * (1 + 0.05);
+                      const weSell = exchangeRate * (1 - 0.05);
+                      
+                      return (
+                      <tr key={currency}>
+                          <td>{currency}</td>
+                          <td>{weBuy.toFixed(2)}</td>
+                          <td>{exchangeRate.toFixed(2)}</td>
+                          <td>{weSell.toFixed(2)}</td>
+                      </tr>
+                      );
+                  })}
+              </tbody>
+              </table>
+          </div>
+          <div className='notes'>
+            <p>
+              Rates are based from 1 USD.<br/>
+              This application uses API from https://currencyfreaks.com/
+            </p>
+          </div>
+        </>
+    );
+};
+
+export default CurrencyExchange;
